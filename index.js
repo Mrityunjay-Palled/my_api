45f34f19480cf6fcabf94d1398f7d0baeeadb347
Mrityunjay-Palled <113463073+Mrityunjay-Palled@users.noreply.github.com>
2023-02-08 23:28:27 +0530

    first commit to feature-1 branch

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import express from "express";
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    console.log('connected to db')
-})
-.catch((err)=>{
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(5000, () => {
   console.log("server started");
